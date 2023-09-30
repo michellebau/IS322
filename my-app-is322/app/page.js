99edc68c4f48d2abb05de68f32ebbea0cfa93e87
michellebau <aermichelle@gmail.com>
2023-09-29 22:30:06 -0400

    updating Toggle component casing

--- my-app-is322/app/page.js
+++ my-app-is322/app/page.js
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './nav.module.css'
-import NightShiftToggle from './toggle'
+import NightShiftToggle from './Toggle'
 import { useState } from 'react';
 
 export default function Home() {
